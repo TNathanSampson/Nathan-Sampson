b52e90e26b9e5616c3f2d4db4f4455e8bf7f2b8e
TNathanSampson <59663555+TNathanSampson@users.noreply.github.com>
2020-02-07 20:18:26 -0600

    index.js

--- index.js
+++ index.js
@@ -29,21 +29,34 @@ console.log(
 // create a variable called "name" that references a string
 const name = "Nathan";
 // create a variable called "favoriteSong" that references a string
-const favoriteSong = "When the Levee Breaks";
+let favoriteSong = "When the Levee Breaks";
 // create a variable called "wage" that references a number
+let wage = 17;
 // create a variable called "age" that references a number
+let age = 35;
 // create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
 // create a variable called "satisfied" that references a boolean
+let satisfied = false;
 // create a variable called "favoriteThing" that references ANY primitive value
+let favoriteThing = 3;
 // print the data type of "favoriteThing"
+console.log(favoriteThing);
+
 // ! You heard a new song that became your favorite.
 // Reassign the variable "favoriteSong"
+favoriteSong = "In My Time of Dying";
 // ! You no longer like the type of data stored in "favoriteThing"
 // Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "Son";
 // ! Happy Birthday!
 // Reassign "age" to a new value
+age = 36;
 // ! You got a big raise at work.
 // Reassign "wage" to a new value
+wage = 20;
 // ! Your raise changed your job satisfaction.
 // Assign "satisfied" to true
+satisfied = true;
 // use "console.log()" and "+" to print a sentence that includes two variables
+console.log(`My name is ${name}, and my favorite song is "${favoriteSong}".`);
