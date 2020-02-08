adc6ae88a4d0105af9862b986d5b7ea24834b32c
TNathanSampson <59663555+TNathanSampson@users.noreply.github.com>
2020-02-07 20:09:26 -0600

    Add comments to notes

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+const myName = "Nathan";
+let numOfYears = 0.2;
+
+//let allows you to re-assign the variable with new data
+numOfYears = 0.3;
+numOfYears = "three tenths";
+
+console.log(myName);
+// this is a comment
+// '.' notation is used to access KEYS within an OBJECT.
+// 'log' is a method.
+
+console.log("My name is " + myName + ".");
+
+//template literals use Interpolation
+console.log(
+  `My name is ${myName}, I have ${numOfYears} years experience with JavaScript.`
+);
+*/
+
+// const x = 3;
+// const y = true;
+// console.log(x);
+// console.log(y);
+
+//null represents the deliberate assignment of a "nothing" value.
+
+// create a variable called "name" that references a string
+const name = "Nathan";
+// create a variable called "favoriteSong" that references a string
+const favoriteSong = "When the Levee Breaks";
+// create a variable called "wage" that references a number
+// create a variable called "age" that references a number
+// create a variable called "onlyChild" that references a boolean
+// create a variable called "satisfied" that references a boolean
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+// ! You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+// ! You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+// ! Happy Birthday!
+// Reassign "age" to a new value
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+// use "console.log()" and "+" to print a sentence that includes two variables
